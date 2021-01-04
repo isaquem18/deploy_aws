fb8aca44f5e591ca501dcef7a164960e9d9cc894
isaque <isaque.financas@gmail.com>
2021-01-04 01:41:02 -0300

    a

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({pagina: 'server is running on aws'});
+});
+
+app.listen(3000);
