c4c0b618b40e50c444855459ef1036e024bd0a6d
isaquem18 <67105969+isaquem18@users.noreply.github.com>
2021-01-14 23:07:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.get('/', (req, res) => {
   return res.json({pagina: 'server is running on aws'});
 });
 
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  return res.json({pagina: id});
+});
+
+
 app.listen(port, () => console.log('server started'));
